7ae4f880a413654849ca91d6c58f24ab6f52e210
Antoine d'Otreppe <a.dotreppe@aspyct.org>
2013-06-02 17:45:37 +0200

    Added the first test class for Maestro

---
+++ test/org/aspyct/orchestra/maestro/MaestroTest.java
@@ -0,0 +1,14 @@
+package org.aspyct.orchestra.maestro;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MaestroTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
