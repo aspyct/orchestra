8fe84e044b65667efa0b3f735fe5563fd729e9b6
Antoine d'Otreppe <a.dotreppe@aspyct.org>
2013-06-05 23:41:24 +0200

    Added the macAddr attribute to the BluetoothDevice class

--- src/org/aspyct/orchestra/api/bluetooth/BluetoothDevice.java
+++ src/org/aspyct/orchestra/api/bluetooth/BluetoothDevice.java
@@ -1,5 +1,18 @@
 package org.aspyct.orchestra.api.bluetooth;
 
-public class BluetoothDevice {
+public class BluetoothDevice implements Comparable<BluetoothDevice> {
+	private String macAddr;
 
+	public String getMacAddr() {
+		return macAddr;
+	}
+
+	public void setMacAddr(String macAddr) {
+		this.macAddr = macAddr;
+	}
+
+	@Override
+	public int compareTo(BluetoothDevice other) {
+		return other.macAddr.compareTo(macAddr);
+	}
 }
